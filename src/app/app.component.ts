98b594826d041c87df111cf19e08af99972c1def
Ovie Omokefe <ovie.omokefe@premiumtrustbank.com>
2024-02-15 15:09:19 +0100

    Initial Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './components/headers/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'CustomerMgr';
+  myList: string[] = ["Ovie", "Monday", "Fejiro"]
 }
